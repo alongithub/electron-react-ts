112a0552fb0da45eb1bdded6f6b246eb99f073c7
孙文龙 <1792108796@qq.com>
2021-12-16 12:01:06 +0800

    加测虚拟机发布状态

--- src/renderer/pages/virtual/components/Publish.jsx
+++ src/renderer/pages/virtual/components/Publish.jsx
@@ -4,23 +4,42 @@ import { message, Button } from 'antd';
 
 const Publish = ({ url, path }) => {
 
-  // let [loading, setLoading] = useState(false);
-  // useEffect(() => {
-  //   axios.get(`http://${url}/isloading`).then(({ data: res }) => {
-  //     if (res.code === 1) {
-  //       setLoading(res.data);
-  //     } else {
-  //       message.error(res.message)
-  //     }
-  //   }).catch(e => {
-  //     message.error('失败，请重试')
-  //   })
-  // }, [])
+
+
+  let [loading, setLoading] = useState(true);
+
+  let getStatus = () => {
+    axios.get(`http://${url}/status`).then(({ data: res }) => {
+      if (res.code === 1) {
+        setLoading(res.data);
+        if (res.data) {
+          setTimeout(getStatus, 5000)
+        }
+      }
+    }).catch(e => {
+      message.error('失败，请检查服务器');
+      setTimeout(getStatus, 5000);
+    })
+
+  }
+
+  useEffect(() => {
+    return () => {
+      getStatus = () => { }
+    }
+  }, [])
+
+  useEffect(() => {
+    if (loading) {
+      getStatus()
+    }
+  }, [loading])
 
   const handlePublish = () => {
     axios.get(`http://${url}${path}`).then(({ data: res }) => {
       if (res.code === 1) {
         message.success(res.message)
+        setLoading(true);
       } else {
         message.error(res.message)
       }
@@ -29,7 +48,7 @@ const Publish = ({ url, path }) => {
     })
   }
 
-  return <Button type="line" onClick={handlePublish}>发布</Button>
+  return <Button loading={loading} type="line" onClick={handlePublish}>发布</Button>
 }
 
 export default Publish;
